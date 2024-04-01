f0715e2ffa884cd8aa74f00ba16bcc41df19ef37
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-04-01 02:33:54 +0200

    *add* MeComponent, tests : - clicking on the back button - click on the delete button

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -4,23 +4,51 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { SessionService } from 'src/app/services/session.service';
 
 import { expect } from '@jest/globals';
 
 import { MeComponent } from './me.component';
+import { of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+
+const mockSessionService = {
+  sessionInformation: {
+    admin: false,
+    id: 1
+  },
+  logOut : jest.fn()
+}
+
+const mockUserService = {
+  getById : (id : string) => of({
+    id: 1,
+    email: 'email@email.com',
+    lastName: 'lastname',
+    firstName: 'firstname',
+    admin: false,
+    password: 'password',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  }),
+  delete : jest.fn((id) => of(void 0))
+}
+
+const snackBarMock = {
+  open : jest.fn()
+}
+
+const routerMock = {
+  navigate : jest.fn((commands : string[]) => null)
+}
 
 describe('MeComponent', () => {
   let component: MeComponent;
   let fixture: ComponentFixture<MeComponent>;
-
-  const mockSessionService = {
-    sessionInformation: {
-      admin: true,
-      id: 1
-    }
-  }
+  
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MeComponent],
@@ -32,18 +60,42 @@ describe('MeComponent', () => {
         MatIconModule,
         MatInputModule
       ],
-      providers: [{ provide: SessionService, useValue: mockSessionService }],
+      providers: [
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: UserService, useValue: mockUserService },
+        { provide: MatSnackBar, useValue : snackBarMock },
+        { provide: Router, useValue: routerMock },
+      ],
     })
       .compileComponents();
 
-    fixture = TestBed.createComponent(MeComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture = TestBed.createComponent(MeComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+    jest.clearAllMocks()
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  
+  // Unit Test
+  it('should go back in history when clicking on the back button', () => {
+    const windowHistorySpy = jest.spyOn(window.history, 'back')
+    const backButton = fixture.debugElement.query(By.css('button[mat-icon-button]'))
+    backButton.triggerEventHandler('click', null)
+    expect(windowHistorySpy).toHaveBeenCalled()
+  })
+
+  describe('if i click on the delete button', () => {
+    it('should try to delete my account, display a related message into a snackbar, logout and redirect me to the homepage', () => {
+      const deleteAccountButton = fixture.debugElement.query(By.css('button[color="warn"]'))
+      deleteAccountButton.triggerEventHandler('click', null)
+      expect(mockUserService.delete).toHaveBeenCalledWith(mockSessionService.sessionInformation.id.toString())
+      expect(snackBarMock.open).toHaveBeenCalledWith("Your account has been deleted !", 'Close', { duration: 3000 })
+      expect(mockSessionService.logOut).toHaveBeenCalled()
+      expect(routerMock.navigate).toHaveBeenCalledWith(['/'])
+    })
+  })
+
 });
