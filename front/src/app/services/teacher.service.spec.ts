3eb050ad17f3873b679a2ba856bfe6f9b9005a7c
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-04-01 02:36:27 +0200

    *add* TeacherService, tests : - when the all method is called - when the detail method is called with a teacher id passed to it

--- front/src/app/services/teacher.service.spec.ts
+++ front/src/app/services/teacher.service.spec.ts
@@ -1,11 +1,31 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { TeacherService } from './teacher.service';
+import { Teacher } from '../interfaces/teacher.interface';
+import { of } from 'rxjs';
+
+const teacher1 : Teacher = {
+  id: 1,
+  lastName: "lastname",
+  firstName: "firstname",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}
+
+const teacher2 : Teacher = {
+  id: 2,
+  lastName: "lastname2",
+  firstName: "firstname2",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+}
 
 describe('TeacherService', () => {
   let service: TeacherService;
+  let httpClient: HttpClient;
+  const pathService = 'api/teacher'
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -14,9 +34,45 @@ describe('TeacherService', () => {
       ]
     });
     service = TestBed.inject(TeacherService);
+    httpClient = TestBed.inject(HttpClient)
+    jest.resetAllMocks()
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('when the all method is called', () => {
+    it('should return an observable broadcasting an array of teachers', () => {
+      const targetEndpoint = `${pathService}`
+
+      jest.spyOn(httpClient, 'get').mockReturnValue(of([{...teacher1}, {...teacher2}]))
+
+      // !!! evaluations into a subscribe callback are ignored
+      let subResponse
+      service.all().subscribe((response) => {
+        subResponse = response
+      })
+      expect(httpClient.get).toHaveBeenCalledWith(targetEndpoint)
+      expect(JSON.stringify(subResponse)).toEqual(JSON.stringify([{...teacher1}, {...teacher2}]))
+    })
+  })
+
+  describe('when the detail method is called with a teacher id passed to it', () => {
+    it('should return an observable broadcasting the infos of the target teacher', () => {
+      const teacherId = '1'
+      const targetEndpoint = `${pathService}/${teacherId}`
+
+      jest.spyOn(httpClient, 'get').mockReturnValue(of(teacher1))
+
+      // !!! evaluations into a subscribe callback are ignored
+      let subResponse
+      service.detail(teacherId).subscribe((response) => {
+        subResponse = response
+      })
+      expect(httpClient.get).toHaveBeenCalledWith(targetEndpoint)
+      expect(JSON.stringify(subResponse)).toEqual(JSON.stringify(teacher1))
+    })
+  })
+
 });
