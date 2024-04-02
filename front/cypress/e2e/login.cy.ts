f76e984d9dde0fc5ba3e4f90d520214a482857cd
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-04-02 13:29:05 +0200

    *add* login, cypress test : failing login

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,14 +1,20 @@
+// @ts-nocheck
 describe('Login spec', () => {
-  it('Login successfull', () => {
+
+  // Successful Login
+  it('Login successful', () => {
     cy.visit('/login')
 
     cy.intercept('POST', '/api/auth/login', {
+      statusCode : 200,
       body: {
-        id: 1,
-        username: 'userName',
-        firstName: 'firstName',
-        lastName: 'lastName',
-        admin: true
+          token: "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ5b2dhQHN0dWRpby5jb20iLCJpYXQiOjE3MTIwMDI4NTEsImV4cCI6MTcxMjA4OTI1MX0.WhlvHw9kw0NPtORRoiZh5_Lm0Ic3r7CvuBJv0w4rvW2ZrRO14OcMiO4MBt-0aQ83-bD0xmuwLT9V0mqvfXRcRw",
+          type: "Bearer",
+          id: 1,
+          username: "yoga@studio.com",
+          firstName: "Admin",
+          lastName: "Admin",
+          admin: true
       },
     })
 
@@ -23,5 +29,33 @@ describe('Login spec', () => {
     cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
 
     cy.url().should('include', '/sessions')
+  })
+
+  // Failing Login
+  it('Login failing', () => {
+    cy.visit('/login')
+
+    cy.intercept('GET', '/api/auth/login', (req) => {
+      req.reply({
+        statusCode: 401,
+        body: {
+          "path": "/api/auth/login",
+          "error": "Unauthorized",
+          "message": "Bad credentials",
+          "status": 401
+        }
+      });
+    }).as('unauthorized');
+
+    cy.get('input[formControlName=email]').type("aaaaaaa@aaaaaaaaaaa")
+    cy.get('input[formControlName=password]').type(`${"test!1234"}`)
+
+    cy.contains('button', 'Submit').click()
+
+    cy.url().should('include', '/login')
+
+    // cy.wait('@unauthorized').then((interception) => {
+      cy.contains('p', 'An error occurred').should('exist')
+    // })
   })
 });
