058908712e1ce0d91070b46fbe89487c656f183a
Cedric B-Eff <cedrikopenclass@gmx.com>
2024-04-13 11:51:28 +0200

    *edit* backend unit tests, session repo : findall, findbyid, deletebyid

--- back/src/test/java/com/openclassrooms/starterjwt/repository/SessionRepositoryTests.java
+++ back/src/test/java/com/openclassrooms/starterjwt/repository/SessionRepositoryTests.java
@@ -2,8 +2,8 @@ package com.openclassrooms.starterjwt.repository;
 
 import com.openclassrooms.starterjwt.models.Session;
 import com.openclassrooms.starterjwt.models.Teacher;
+import com.openclassrooms.starterjwt.models.User;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +11,7 @@ import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -46,41 +47,75 @@ public class SessionRepositoryTests {
                 .lastName(teacher2Ln)
                 .build();
 
-        session1 = Session.builder().name("session_name1").description("session_description1").date(new Date()).teacher(teacher1)
+        session1 = Session.builder().id(1L).name("session_name1").description("session_description1").date(new Date()).teacher(teacher1)
                 .build();
 
-        session2 = Session.builder().name("session_name2").description("session_description2").date(new Date()).teacher(teacher2)
+        session2 = Session.builder().id(2L).name("session_name2").description("session_description2").date(new Date()).teacher(teacher2)
                 .build();
 
         teacherRepository.save(teacher1);
         teacherRepository.save(teacher2);
+        sessionRepository.save(session1);
     }
 
     @Test
-    public void givenSessionObject_whenSave_thenReturnSavedSession(){
+    void findById_SessionExists_ReturnAUser(){
+        // Act
+        Session session = sessionRepository.findById(1L).orElse(null);
+        // Assert
+        assertThat(session).isNotNull();
+        assertThat(session.getId()).isEqualTo(1L);
+        assertThat(session.getName()).isEqualTo(session1.getName());
+        assertThat(session.getDescription()).isEqualTo(session1.getDescription());
+    }
 
-        // when - action or the behaviour that we are going test
-        Session savedSession = sessionRepository.save(session1);
+    @Test
+    void findById_SessionDoesntExist_ReturnAnEmptyOptional(){
+        // Act
+        Optional<Session> sessionOptional = sessionRepository.findById(3L);
+        // Assert
+        assertThat(sessionOptional.isEmpty()).isTrue();
+    }
 
-        // then - verify the output
+    @Test
+    void findAll_MultipleSessionsExists_ReturnAnArrayOfSessions(){
+        // Act
+        sessionRepository.save(session2);
+        List<Session> sessionList = sessionRepository.findAll();
+        // Assert
+        assertThat(sessionList.size()).isEqualTo(2);
+        assertThat(sessionList.get(0).getName()).isEqualTo(session1.getName());
+        assertThat(sessionList.get(1).getName()).isEqualTo(session2.getName());
+        assertThat(sessionList.get(0).getDescription()).isEqualTo(session1.getDescription());
+        assertThat(sessionList.get(1).getDescription()).isEqualTo(session2.getDescription());
+    }
+
+    @Test
+    void findAll_NoExistingSessions_ReturnAnEmptyArray(){
+        // Act
+        sessionRepository.deleteAll();
+        List<Session> sessionList = sessionRepository.findAll();
+        // Assert
+        assertThat(sessionList.size()).isEqualTo(0);
+    }
+
+    @Test
+    void saveSession(){
+        // Act
+        Session savedSession = sessionRepository.save(session2);
+        // Assert
         assertThat(savedSession).isNotNull();
-        assertThat(savedSession.getName()).isEqualTo("session_name1");
-        assertThat(savedSession.getDescription()).isEqualTo("session_description1");
-        assertThat(savedSession.getTeacher()).isEqualTo(teacher1);
+        assertThat(savedSession.getId()).isEqualTo(2L);
+        assertThat(savedSession.getName()).isEqualTo(session2.getName());
+        assertThat(savedSession.getDescription()).isEqualTo(session2.getDescription());
     }
 
     @Test
-    public void givenSessionsObject_whenSave_thenReturnSavedSession(){
-
-        // when - action or the behaviour that we are going test
-        Session savedSession1 = sessionRepository.save(session1);
-        Session savedSession2 = sessionRepository.save(session2);
-
-        // then - verify the output
-        assertThat(savedSession1).isNotNull();
-        assertThat(savedSession2).isNotNull();
-        List<Session> sessionsList = sessionRepository.findAll();
-        assertThat(sessionsList).isNotNull();
-        assertThat(sessionsList.size()).isEqualTo(2);
+    void deleteById(){
+        Session retrievedSession = sessionRepository.findById(1L).orElse(null);
+        assertThat(retrievedSession).isNotNull();
+        sessionRepository.deleteById(1L);
+        Optional<Session> sessionOptional = sessionRepository.findById(1L);
+        assertThat(sessionOptional.isEmpty()).isTrue();
     }
 }
